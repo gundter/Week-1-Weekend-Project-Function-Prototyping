5caa0f5f65cfe5b13b5fedd94629dd94aeb0fbf9
Terry Gunderson <tgunderson25@gmail.com>
2015-04-19 18:45:21 -0500

    Updated objects

--- app.js
+++ app.js
@@ -2,16 +2,45 @@ var arrayUser = [];
 var attackBonus;
 var i;
 
-document.getElementById('addStats').addEventListener('click', makePlayer);
-//document.getElementById().addEventListener('click', fight);
+document.getElementById('addStats').addEventListener('click', addStats);
+document.getElementById('fight').addEventListener('click', fight);
 
-function makePlayer(){
-	arrayUser.push(new Monster("Player"));
-	arrayUser.push(new Monster("Monster"));
+function addStats (){
+	console.log("Entering addStats function");
+	//|-----------------Creating Monster-----------------------------|
+var Monster = {
+	type : "Monster",
+	strength : parseInt(document.getElementById('strength').elements[0].value),
+	dexterity : parseInt(document.getElementById('dexterity').elements[0].value),
+	constitution : parseInt(document.getElementById('constitution').elements[0].value),
+	wisdom : parseInt(document.getElementById('wisdom').elements[0].value),
+	intelligence : parseInt(document.getElementById('intelligence').elements[0].value),
+	charisma : parseInt(document.getElementById('charisma').elements[0].value),
+	attack : function(){
+		if (this.type == "Monster"){
+			attackBonus = rollDie();
+			this.attBonus = attackBonus;
+			this.attackTotal = attackBonus + this.strength;
+			return this.attackTotal;
+		}else if (this.type == "Player"){
+			attackBonus = rollDie();
+			this.attBonus = attackBonus;
+			this.attackTotal = attackBonus + this.dexterity;
+			return this.attackTotal;
+		}
+	}
+}
+//|---------------Creating Player--------------------------------|
+var Player = Object.create( Monster );
+Player.type = "Player";
+Player.enteredName = document.getElementById('name').elements[0].value;
 
-	for (i=0; i < arrayUser.length; i++){
+arrayUser.push(Player);
+arrayUser.push(Monster);
+
+for (i=0; i < arrayUser.length; i++){
 		document.getElementById('name').reset();
-		document.getElementById('pMName').innerHTML = arrayUser[i].name;
+		document.getElementById('userName').innerHTML = arrayUser[i].enteredName;
 
 		document.getElementById('strength').reset();
 		document.getElementById('userStr').innerHTML = arrayUser[i].strength;
@@ -30,41 +59,28 @@ function makePlayer(){
 
 		document.getElementById('charisma').reset();
 		document.getElementById('userChar').innerHTML = arrayUser[i].charisma;
-	}
 }
 
-function Monster (name) {
-	this.name = name;
-	this.strength = parseInt(document.getElementById('strength').elements[0].value);
-	this.dexterity = parseInt(document.getElementById('dexterity').elements[0].value);;
-	this.constitution = parseInt(document.getElementById('constitution').elements[0].value);;
-	this.wisdom = parseInt(document.getElementById('wisdom').elements[0].value);;
-	this.intelligence = parseInt(document.getElementById('intelligence').elements[0].value);;
-	this.charisma = parseInt(document.getElementById('charisma').elements[0].value);;
-	this.attack = function(){
-		if (this.name == "Monster"){
-			attackBonus = rollDie();
-			this.attBonus = attackBonus;
-			this.attackTotal = attackBonus + this.strength;
-			return this.attackTotal;
-		}else if (this.name == "Player"){
-			attackBonus = rollDie();
-			this.attBonus = attackBonus;
-			this.attackTotal = attackBonus + this.dexterity;
-			return this.attackTotal;
+}
+
+function fight(){
+	for (i = 0; i <arrayUser.length -1; i++){
+		var playerAttack = arrayUser[i].attack;
+		var monsterAttack = arrayUser[i+1].attack;
+
+		if (monsterAttack > playerAttack){
+			document.getElementById('result').innerHTML = "The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The Monster wins!";
+			//alert("The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The Monster wins!");
+		}else if (monsterAttack < playerAttack){
+			document.getElementById('result').innerHTML = "The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The Player wins!";
+			//alert("The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The Player wins!");
+		}else if (monsterAttack == playerAttack){
+			document.getElementById('result').innerHTML = "The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The result is a tie!";
+			//alert("The Monster's attack is: " + monsterAttack +"!/n" +"The Player's attack is: " + playerAttack + "!/n" + "The result is a tie!");
 		}
 	}
 }
 
-function fight(){
-	for (i = 0; i <arrayUser.length - 1; i++){
-		var playerAttack = arrayUser[i].attack;
-		var monsterAttack = arrayUser[i+1].attack;
-
-		
-	}
-}
-
 function rollDie(){
-	return Math.floor(Math.random() *20) + 1;
+	return Math.floor(Math.random() * 20) + 1;
 }
